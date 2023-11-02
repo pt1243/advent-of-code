e2b3e82fd1614bc85f24dd1707676ffd2fa416b2
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-11-02 17:14:59 +0100

    2015 day 9

---
+++ 2015/9.py
@@ -0,0 +1,53 @@
+from itertools import pairwise, permutations
+
+
+with open('./2015/resources/9.txt') as f:
+    all_distances = [line.strip() for line in f]
+
+
+def problem_1():
+    destinations: set[str] = set()
+    distances: dict[tuple[str, str], int] = {}
+    for entry in all_distances:
+        locations, _, dist = entry.partition(" = ")
+        from_place, _, to_place = locations.partition(" to ")
+        destinations.add(from_place)
+        destinations.add(to_place)
+        distances[(from_place, to_place)] = int(dist)
+    
+    shortest_distance: int | None = None
+    for route in permutations(destinations):
+        route_total = 0
+        for start, end in pairwise(route):
+            try:
+                route_total += distances[(start, end)]
+            except KeyError:
+                route_total += distances[(end, start)]
+        if shortest_distance is None or route_total < shortest_distance:
+            shortest_distance = route_total
+    
+    print(shortest_distance)
+
+
+def problem_2():
+    destinations: set[str] = set()
+    distances: dict[tuple[str, str], int] = {}
+    for entry in all_distances:
+        locations, _, dist = entry.partition(" = ")
+        from_place, _, to_place = locations.partition(" to ")
+        destinations.add(from_place)
+        destinations.add(to_place)
+        distances[(from_place, to_place)] = int(dist)
+    
+    longest_distance: int | None = None
+    for route in permutations(destinations):
+        route_total = 0
+        for start, end in pairwise(route):
+            try:
+                route_total += distances[(start, end)]
+            except KeyError:
+                route_total += distances[(end, start)]
+        if longest_distance is None or route_total > longest_distance:
+            longest_distance = route_total
+    
+    print(longest_distance)
