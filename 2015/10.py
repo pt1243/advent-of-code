369fe0c97431390df7a351c74cfdc8abcface2bd
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-11-02 17:33:08 +0100

    2015 day 10

---
+++ 2015/10.py
@@ -0,0 +1,23 @@
+from more_itertools import split_when
+
+
+def problem_1():
+    val = "1113122113"
+    for _ in range(40):
+        new_val = ""
+        for same_digits in split_when(val, lambda x, y: x != y):
+            new_val += str(len(same_digits))
+            new_val += same_digits[0]
+        val = new_val
+    print(len(val))
+
+
+def problem_2():
+    val = "1113122113"
+    for _ in range(50):
+        new_val = ""
+        for same_digits in split_when(val, lambda x, y: x != y):
+            new_val += str(len(same_digits))
+            new_val += same_digits[0]
+        val = new_val
+    print(len(val))
