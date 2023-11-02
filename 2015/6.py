68a67422faa27f92b0393d4ae80a01968f9bfc54
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-11-02 13:48:48 +0100

    2015 day 6

---
+++ 2015/6.py
@@ -0,0 +1,44 @@
+def problem_1():
+    import numpy as np
+    with open('./2015/resources/6.txt') as f:
+        instructions = [line.strip() for line in f]
+
+    lights = np.zeros((1000, 1000), dtype=bool)
+    for instruction in instructions:
+        start_str, _, end_str = instruction.partition(" through ")
+        end_x, end_y = tuple(int(i) for i in end_str.split(','))
+        start_x, start_y = tuple(int(i) for i in start_str.rsplit(" ", maxsplit=1)[1].split(","))
+
+        subarray = lights[start_x:end_x+1, start_y:end_y+1]
+
+        if instruction.startswith("turn on"):
+            lights[start_x:end_x+1, start_y:end_y+1] = np.ones_like(subarray)
+        elif instruction.startswith("turn off"):
+            lights[start_x:end_x+1, start_y:end_y+1] = np.zeros_like(subarray)
+        else:
+            lights[start_x:end_x+1, start_y:end_y+1] = np.invert(subarray)
+        
+    print(np.sum(lights))
+
+
+def problem_2():
+    import numpy as np
+    with open('./2015/resources/6.txt') as f:
+        instructions = [line.strip() for line in f]
+
+    lights = np.zeros((1000, 1000), dtype=int)
+    for instruction in instructions:
+        start_str, _, end_str = instruction.partition(" through ")
+        end_x, end_y = tuple(int(i) for i in end_str.split(','))
+        start_x, start_y = tuple(int(i) for i in start_str.rsplit(" ", maxsplit=1)[1].split(","))
+
+        subarray = lights[start_x:end_x+1, start_y:end_y+1]
+
+        if instruction.startswith("turn on"):
+            lights[start_x:end_x+1, start_y:end_y+1] = subarray + 1
+        elif instruction.startswith("turn off"):
+            lights[start_x:end_x+1, start_y:end_y+1] = np.maximum(np.zeros_like(subarray), subarray - 1)
+        else:
+            lights[start_x:end_x+1, start_y:end_y+1] = subarray + 2
+        
+    print(np.sum(lights))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 year = 2015
-day = 5
+day = 6
 problem = 2
 
 
