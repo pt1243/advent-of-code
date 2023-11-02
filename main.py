44c96e95f6092df91d8d6e34aa4f3444b8228887
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-11-02 12:21:47 +0100

    2015 day 1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from time import perf_counter
 
 year = 2015
 day = 1
-problem = 1
+problem = 2
 
 
 def main():
@@ -16,4 +16,4 @@ def main():
 if __name__ == "__main__":
     start = perf_counter()
     main()
-    print(f"Day {day}, problem {problem} completed in {perf_counter() - start:.3f}s")
+    print(f"{year} day {day}, problem {problem} completed in {perf_counter() - start:.3f}s")
