d6e7ef0af1dade8d9d52b1ea9d8898fcb93df86f
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-11-03 03:24:14 +0100

    2015 day 17

---
+++ 2015/17.py
@@ -0,0 +1,31 @@
+from itertools import product
+
+
+with open('./2015/resources/17.txt') as f:
+    sizes = [int(line.strip()) for line in f]
+
+
+def problem_1():
+    total = 0
+    for comb in product((True, False), repeat=len(sizes)):
+        comb_total = 0
+        for idx, include in enumerate(comb):
+            if include:
+                comb_total += sizes[idx]
+        if comb_total == 150:
+            total += 1
+    
+    print(total)
+
+
+def problem_2():
+    num_ways = {i: 0 for i in range(len(sizes) + 1)}
+    for comb in product((True, False), repeat=len(sizes)):
+        comb_total = 0
+        for idx, include in enumerate(comb):
+            if include:
+                comb_total += sizes[idx]
+        if comb_total == 150:
+             num_ways[sum(comb)] += 1
+    
+    print(num_ways[min({k: v for k, v in num_ways.items() if v > 0})])
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 year = 2015
-day = 16
+day = 17
 problem = 2
 
 
