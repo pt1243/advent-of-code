ca426fb56b515195a8d001a284ca49609d9c508d
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-11-10 15:46:11 +0100

    2015 day 21

---
+++ 2015/21.py
@@ -0,0 +1,87 @@
+from itertools import product, combinations
+from typing import NamedTuple
+
+
+class Item(NamedTuple):
+    name: str
+    cost: int
+    damage: int
+    armour: int
+
+
+weapon_choices = (
+    Item("Dagger", 8, 4, 0),
+    Item("Shortsword", 10, 5, 0),
+    Item("Warhammer", 25, 6, 0),
+    Item("Longsword", 40, 7, 0),
+    Item("Greataxe", 74, 8, 0),
+)
+
+
+armour_choices = (
+    Item("No armour", 0, 0, 0),
+    Item("Leather", 13, 0, 1),
+    Item("Chainmail", 31, 0, 2),
+    Item("Splintmail", 53, 0, 3),
+    Item("Bandedmail", 75, 0, 4),
+    Item("Platemail", 102, 0, 5),
+)
+
+
+rings = (
+    Item("Damage +1", 25, 1, 0),
+    Item("Damage +2", 50, 2, 0),
+    Item("Damage +3", 100, 3, 0),
+    Item("Defense +1", 20, 0, 1),
+    Item("Defense +2", 40, 0, 2),
+    Item("Defense +3", 80, 0, 3),
+)
+
+
+ring_choices = (
+    Item("No rings", 0, 0, 0),
+    *(r for r in rings),  # one ring
+    *(Item(r1.name + " and " + r2.name, r1.cost + r2.cost, r1.damage + r2.damage, r1.armour + r2.armour) for r1, r2 in combinations(rings, 2)),
+)
+
+
+def simulate(player_hp: int, player_damage: int, player_armour: int, boss_hp: int, boss_damage: int, boss_armour: int) -> bool:
+        current_turn = "player"
+        while boss_hp > 0 and player_hp > 0:
+            if current_turn == "player":
+                damage = max(player_damage - boss_armour, 1)
+                boss_hp -= damage
+                current_turn = "boss"
+            else:
+                damage = max(boss_damage - player_armour, 1)
+                player_hp -= damage
+                current_turn = "player"
+        return player_hp > 0
+
+
+def problem_1():    
+    min_cost = float('inf')
+    for weapon, armour, ring_combo in product(weapon_choices, armour_choices, ring_choices):
+        total_cost = weapon.cost + armour.cost + ring_combo.cost
+        total_damage = weapon.damage + armour.damage + ring_combo.damage
+        total_armour = weapon.armour + armour.armour + ring_combo.armour
+
+        if simulate(100, total_damage, total_armour, 104, 8, 1):
+            if total_cost < min_cost:
+                min_cost = total_cost
+
+    print(min_cost)
+
+
+def problem_2():
+    max_cost = 0
+    for weapon, armour, ring_combo in product(weapon_choices, armour_choices, ring_choices):
+        total_cost = weapon.cost + armour.cost + ring_combo.cost
+        total_damage = weapon.damage + armour.damage + ring_combo.damage
+        total_armour = weapon.armour + armour.armour + ring_combo.armour
+
+        if not simulate(100, total_damage, total_armour, 104, 8, 1):
+            if total_cost > max_cost:
+                max_cost = total_cost
+
+    print(max_cost)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 year = 2015
-day = 20
+day = 21
 problem = 2
 
 
