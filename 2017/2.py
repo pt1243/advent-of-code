716936dfac145a7a87dc850bc9ccca3f2fc30ce2
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-12-04 22:30:15 +0100

    2017 day 2

---
+++ 2017/2.py
@@ -0,0 +1,30 @@
+with open('./2017/resources/2.txt') as f:
+    lines = [line.strip() for line in f]
+
+
+def problem_1() -> None:
+    total_checksum = 0
+    for line in lines:
+        nums = [int(n) for n in line.split()]
+        total_checksum += max(nums) - min(nums)
+    print(total_checksum)
+
+
+def problem_2() -> None:
+    total = 0
+    for line in lines:
+        nums = [int(n) for n in line.split()]
+        found = False
+        for i, num_1 in enumerate(nums):
+            for num_2 in nums[i+1:]:
+                if num_1 % num_2 == 0:
+                    total += num_1 // num_2
+                    found = True
+                    break
+                if num_2 % num_1 == 0:
+                    total += num_2 // num_1
+                    found = True
+                    break
+            if found:
+                break
+    print(total)
