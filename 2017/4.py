878635925dab5a42ccedb263ec4bdb29a33c9fe3
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-12-07 03:15:31 +0100

    2017 day 4

---
+++ 2017/4.py
@@ -0,0 +1,20 @@
+with open('./2017/resources/4.txt') as f:
+    lines = [line.strip() for line in f]
+
+
+def problem_1() -> None:
+    total_valid = 0
+    for line in lines:
+        words = line.split()
+        if len(words) == len(set(words)):
+            total_valid += 1
+    print(total_valid)
+
+
+def problem_2() -> None:
+    total_valid = 0
+    for line in lines:
+        words_letters_sorted = ["".join(sorted(word)) for word in line.split()]
+        if len(words_letters_sorted) == len(set(words_letters_sorted)):
+            total_valid += 1
+    print(total_valid)
