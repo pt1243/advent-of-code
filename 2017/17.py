b93edd93fbdb7d1570919db59aa89ee95adbb542
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-12-12 04:35:55 +0100

    2017 day 17

---
+++ 2017/17.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+n_times = 354
+
+
+def spinlock(n: int, iterations: int) -> list[int]:
+    lst = deque([0])
+    for i in range(1, iterations + 1):
+        lst.rotate(-n)
+        lst.append(i)
+    return lst
+
+
+def problem_1() -> None:
+    result = spinlock(n_times, 2017)
+    print(result[0])
+
+
+def problem_2() -> None:
+    result = spinlock(n_times, 50_000_000)
+    print(result[result.index(0) + 1])
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import importlib
 from time import perf_counter
 
 
-year = 2023
-day = 11
+year = 2017
+day = 17
 problem = 2
 
 
