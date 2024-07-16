5e9be35bc09ef2c5e1e13c1a4339ca05ae9e4988
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-07-16 22:45:25 +1000

    2016 day 22, part 2

--- 2016/22.py
+++ 2016/22.py
@@ -36,3 +36,33 @@ def problem_1() -> None:
             if a.used <= b.avail:
                 found_viable_pairs += 1
     print(found_viable_pairs)
+
+
+def problem_2() -> None:
+    all_nodes: list[Node] = []
+    for line in lines:
+        split = line.split()
+        all_nodes.append(
+            Node(
+                int(split[0][split[0].index("-") + 2 : split[0].rindex("-")]),
+                int(split[0][split[0].rindex("-") + 2 :]),
+                int(split[1][:-1]),
+                int(split[2][:-1]),
+                int(split[3][:-1]),
+            )
+        )
+    max_x = max((node.x for node in all_nodes))
+    
+    large_size_threshold = 400
+    large_node_positions = [(node.x, node.y) for node in all_nodes if node.size > large_size_threshold]
+    empty_position = [(node.x, node.y) for node in all_nodes if node.used == 0][0]
+
+    large_positions_y = large_node_positions[0][1]
+    large_positions_start_x = sorted(large_node_positions)[0][0]
+    large_positions_end_x = sorted(large_node_positions)[-1][0]
+
+    steps_to_move_next_to_g = max_x - empty_position[0] + empty_position[1]
+    if empty_position[1] > large_positions_y and large_positions_start_x <= empty_position[0]:
+        steps_to_move_next_to_g += 2 * (empty_position[0] - large_positions_start_x + 1)
+    g_starting_dist = max_x - 1
+    print(steps_to_move_next_to_g + 5 * g_starting_dist)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 year = 2016
-day = 11
+day = 22
 problem = 2
 
 
