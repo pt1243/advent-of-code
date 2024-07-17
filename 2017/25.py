413f0cce8952952fc7cb3a4e6b4c39ab58cd1b21
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-07-18 01:30:27 +1000

    remove extra test input

--- 2017/25.py
+++ 2017/25.py
@@ -2,30 +2,6 @@ from collections import defaultdict
 from typing import NamedTuple
 
 
-blueprint = """\
-Begin in state A.
-Perform a diagnostic checksum after 6 steps.
-
-In state A:
-  If the current value is 0:
-    - Write the value 1.
-    - Move one slot to the right.
-    - Continue with state B.
-  If the current value is 1:
-    - Write the value 0.
-    - Move one slot to the left.
-    - Continue with state B.
-
-In state B:
-  If the current value is 0:
-    - Write the value 1.
-    - Move one slot to the left.
-    - Continue with state A.
-  If the current value is 1:
-    - Write the value 1.
-    - Move one slot to the right.
-    - Continue with state A."""
-
 with open("./2017/resources/25.txt") as f:
     blueprint = f.read().strip()
 
