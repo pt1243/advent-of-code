c9cf46da35c745c4f0673d56e758234aadf8251c
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-08-15 03:03:00 +1000

    fix 2015 day 25

--- 2015/25.py
+++ 2015/25.py
@@ -1,22 +1,22 @@
-from typing import cast
-
 import numpy as np
-import numpy.typing as npt
 
 
-def get_code(row: int, col: int) -> int:
-    diagonal = row + col - 1
-    row -= 1
-    col -= 1
-    codes = cast(npt.NDArray[np.int_], np.zeros((diagonal, diagonal), dtype=int))
-    codes[0, 0] = 20151125
-
-    for d in range(1, diagonal):
-        codes[d, 0] = codes[0, d - 1] * 252533 % 33554393
-        for i in range(1, d + 1):
-            codes[d - i, i] = codes[d - i + 1, i - 1] * 252533 % 33554393
-    return cast(int, codes[row, col])
+with open("./2015/resources/25.txt") as f:
+    console_message = f.read().strip()
 
 
 def problem_1() -> None:
-    print(get_code(2978, 3083))
+    split = console_message.split()
+    row, col = int(split[15].strip(",")), int(split[17].strip("."))
+
+    diagonal = row + col - 1
+    row -= 1
+    col -= 1
+    codes = np.zeros((diagonal, diagonal), dtype=int)
+    codes[0, 0] = 20151125
+
+    for d in range(1, diagonal):
+        codes[d, 0] = int(codes[0, d - 1]) * 252533 % 33554393
+        for i in range(1, d + 1):
+            codes[d - i, i] = int(codes[d - i + 1, i - 1]) * 252533 % 33554393
+    print(codes[row, col])
