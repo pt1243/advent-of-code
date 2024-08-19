92001c32fb0861892ff038290c4f38f0dc6f5b5c
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-08-19 23:58:27 +1000

    2020 day 3

---
+++ 2020/03.py
@@ -0,0 +1,27 @@
+from itertools import batched
+
+
+with open("./2020/resources/3.txt") as f:
+    lines = f.read().splitlines()
+
+
+def problem_1() -> None:
+    width = len(lines[0])
+    trees_hit = 0
+    for i, row in enumerate(lines):
+        if row[(i * 3) % width] == "#":
+            trees_hit += 1
+    print(trees_hit)
+
+
+def problem_2() -> None:
+    width = len(lines[0])
+    prod = 1
+    for slope in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
+        trees_hit = 0
+        for i, row_group in enumerate(batched(lines, slope[1])):
+            row = row_group[0]
+            if row[(i * slope[0]) % width] == "#":
+                trees_hit += 1
+        prod *= trees_hit
+    print(prod)
