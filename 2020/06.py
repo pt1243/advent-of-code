ab3f27ac3195a9490d3529169651bd0080a4f32b
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-08-20 00:37:50 +1000

    2020 day 6

---
+++ 2020/06.py
@@ -0,0 +1,18 @@
+with open("./2020/resources/6.txt") as f:
+    text = f.read().strip()
+
+
+def problem_1() -> None:
+    sum_of_counts = 0
+    for group in text.split("\n\n"):
+        group = group.replace("\n", "")
+        sum_of_counts += len(set(group))
+    print(sum_of_counts)
+
+
+def problem_2() -> None:
+    sum_of_counts = 0
+    for group in text.split("\n\n"):
+        people = [set(line) for line in group.split()]
+        sum_of_counts += len(people[0].intersection(*people[1:]))
+    print(sum_of_counts)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 year = 2020
-day = 5
+day = 6
 problem = 2
 
 
