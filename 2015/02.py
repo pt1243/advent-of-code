30dcfcfcd9502a878b359fd4e17e29ef8e5d30d7
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-11-18 23:46:26 +0100

    remove E741 exception

--- 2015/02.py
+++ 2015/02.py
@@ -5,10 +5,10 @@ with open("./2015/resources/2.txt") as f:
 def problem_1() -> None:
     total = 0
     for line in boxes:
-        l, w, h = (int(i) for i in line.split("x"))
-        lw = l * w
-        lh = l * h
-        wh = w * h
+        length, width, height = (int(i) for i in line.split("x"))
+        lw = length * width
+        lh = length * height
+        wh = width * height
         total += 2 * lw + 2 * lh + 2 * wh
         total += min(lw, lh, wh)
     print(total)
@@ -17,10 +17,10 @@ def problem_1() -> None:
 def problem_2() -> None:
     total = 0
     for line in boxes:
-        l, w, h = (int(i) for i in line.split("x"))
-        perimeter_lw = 2 * l + 2 * w
-        perimeter_lh = 2 * l + 2 * h
-        perimeter_wh = 2 * w + 2 * h
+        length, width, height = (int(i) for i in line.split("x"))  # noqa: E741
+        perimeter_lw = 2 * length + 2 * width
+        perimeter_lh = 2 * length + 2 * height
+        perimeter_wh = 2 * width + 2 * height
         total += min(perimeter_lw, perimeter_lh, perimeter_wh)
-        total += l * w * h
+        total += length * width * height
     print(total)
