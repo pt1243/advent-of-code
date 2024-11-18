f15d543d61e1c0031ccd1fa89a7d612476038bfe
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-11-18 15:07:19 +0100

    2021 day 3

---
+++ 2021/03.py
@@ -0,0 +1,39 @@
+from collections import Counter
+
+
+with open("./2021/resources/3.txt") as f:
+    lines = f.readlines()
+
+
+def problem_1() -> None:
+    column_counters = [Counter() for _ in lines[0]]
+    for line in lines:
+        for c, char in zip(column_counters, line):
+            c.update(char)
+    gamma_rate = int("".join(c.most_common(1)[0][0] for c in column_counters), 2)
+    epsilon_rate = int("".join(c.most_common()[-1][0] for c in column_counters), 2)
+    print(gamma_rate * epsilon_rate)
+
+
+def problem_2() -> None:
+    position = 0
+    lines_to_keep = lines.copy()
+    while True:
+        most, least = Counter(line[position] for line in lines_to_keep).most_common()
+        value_to_keep = "1" if most[1] == least[1] else most[0]
+        lines_to_keep = [line for line in lines_to_keep if line[position] == value_to_keep]
+        if len(lines_to_keep) == 1:
+            oxygen_generator_rating = int(lines_to_keep[0], 2)
+            break
+        position += 1
+    position = 0
+    lines_to_keep = lines.copy()
+    while True:
+        most, least = Counter(line[position] for line in lines_to_keep).most_common()
+        value_to_keep = "0" if most[1] == least[1] else least[0]
+        lines_to_keep = [line for line in lines_to_keep if line[position] == value_to_keep]
+        if len(lines_to_keep) == 1:
+            co2_scrubber_rating = int(lines_to_keep[0], 2)
+            break
+        position += 1
+    print(oxygen_generator_rating * co2_scrubber_rating)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 year = 2021
-day = 2
+day = 3
 problem = 2
 
 
