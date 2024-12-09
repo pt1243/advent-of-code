8749a5282241549140357175d9c0c16c36b64356
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-10 00:06:08 +0100

    switch to faster recursive solution

--- 2024/07.py
+++ 2024/07.py
@@ -1,10 +1,25 @@
-from functools import reduce
-from itertools import product
-from operator import add, mul
+from collections.abc import Sequence
+from math import floor, log10
 
 
 with open("./2024/resources/7.txt") as f:
     lines = f.read().splitlines()
+
+
+def can_reach(nums: Sequence[int], target: int, use_conat: bool = False) -> bool:
+    if len(nums) == 1:
+        return target == nums[0]
+    last = nums[-1]
+    quotient, remainder = divmod(target, last)
+    if remainder == 0 and can_reach(nums[:-1], quotient, use_conat):
+        return True
+    if last <= target and can_reach(nums[:-1], target - last, use_conat):
+        return True
+    if use_conat:
+        digits_in_last = 1 + floor(log10(last))
+        if target % (10**digits_in_last) == last:
+            return can_reach(nums[:-1], target // (10**digits_in_last), use_conat)
+    return False
 
 
 def problem_1() -> None:
@@ -13,11 +28,8 @@ def problem_1() -> None:
         left, right = line.split(": ")
         target = int(left)
         nums = [int(x) for x in right.split()]
-        for operations in product((add, mul), repeat=len(nums) - 1):
-            ops = iter(operations)
-            if reduce(lambda x, y: next(ops)(x, y), nums) == target:
-                total += target
-                break
+        if can_reach(nums, target):
+            total += target
     print(total)
 
 
@@ -27,9 +39,6 @@ def problem_2() -> None:
         left, right = line.split(": ")
         target = int(left)
         nums = [int(x) for x in right.split()]
-        for operations in product((add, mul, lambda x, y: int(str(x) + str(y))), repeat=len(nums) - 1):
-            ops = iter(operations)
-            if reduce(lambda x, y: next(ops)(x, y), nums) == target:
-                total += target
-                break
+        if can_reach(nums, target, use_conat=True):
+            total += target
     print(total)
