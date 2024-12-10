865587852fef1ec9d75f0784388c20a3eea06136
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-10 11:05:29 +0100

    refactor into common function

--- 2024/10.py
+++ 2024/10.py
@@ -2,62 +2,42 @@ with open("./2024/resources/10.txt") as f:
     grid = [[int(x) for x in line] for line in f.read().splitlines()]
 
 
-def problem_1() -> None:
+def find_paths(start_row: int, start_col: int) -> tuple[int, int]:
     width, height = len(grid[0]), len(grid)
+    open_set = [(start_row, start_col)]
+    num_paths = 0
+    end_points: set[tuple[int, int]] = set()
+    while open_set:
+        row, col = open_set.pop()
+        val = grid[row][col]
+        if val == 9:
+            num_paths += 1
+            end_points.add((row, col))
+            continue
+        if row > 0 and grid[row - 1][col] == val + 1:
+            open_set.append((row - 1, col))
+        if row < height - 1 and grid[row + 1][col] == val + 1:
+            open_set.append((row + 1, col))
+        if col > 0 and grid[row][col - 1] == val + 1:
+            open_set.append((row, col - 1))
+        if col < width - 1 and grid[row][col + 1] == val + 1:
+            open_set.append((row, col + 1))
+    return len(end_points), num_paths
 
-    def search(start_row: int, start_col: int) -> int:
-        open_set = [(start_row, start_col)]
-        found: set[tuple[int, int]] = set()
-        while open_set:
-            row, col = open_set.pop()
-            val = grid[row][col]
-            if val == 9:
-                found.add((row, col))
-                continue
-            if row > 0 and grid[row - 1][col] == val + 1:
-                open_set.append((row - 1, col))
-            if row < height - 1 and grid[row + 1][col] == val + 1:
-                open_set.append((row + 1, col))
-            if col > 0 and grid[row][col - 1] == val + 1:
-                open_set.append((row, col - 1))
-            if col < width - 1 and grid[row][col + 1] == val + 1:
-                open_set.append((row, col + 1))
-        return len(found)
 
+def problem_1() -> None:
     total = 0
     for row, line in enumerate(grid):
         for col, num in enumerate(line):
             if num == 0:
-                total += search(row, col)
+                total += find_paths(row, col)[0]
     print(total)
 
 
 def problem_2() -> None:
-    width, height = len(grid[0]), len(grid)
-
-    def num_paths(start_row: int, start_col: int) -> int:
-        open_set = [[(start_row, start_col)]]
-        found: set[tuple[tuple[int, int], ...]] = set()
-        while open_set:
-            path = open_set.pop()
-            row, col = path[-1]
-            val = grid[row][col]
-            if val == 9:
-                found.add(tuple(path))
-                continue
-            if row > 0 and grid[row - 1][col] == val + 1:
-                open_set.append(path + [(row - 1, col)])
-            if row < height - 1 and grid[row + 1][col] == val + 1:
-                open_set.append(path + [(row + 1, col)])
-            if col > 0 and grid[row][col - 1] == val + 1:
-                open_set.append(path + [(row, col - 1)])
-            if col < width - 1 and grid[row][col + 1] == val + 1:
-                open_set.append(path + [(row, col + 1)])
-        return len(found)
-
     total = 0
     for row, line in enumerate(grid):
         for col, num in enumerate(line):
             if num == 0:
-                total += num_paths(row, col)
+                total += find_paths(row, col)[1]
     print(total)
