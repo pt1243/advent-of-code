2a22a4f276131106da7543f46b659e1c5283c39a
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-14 07:24:47 +0100

    use regex to clean up input parsing

--- 2024/13.py
+++ 2024/13.py
@@ -1,3 +1,4 @@
+import re
 from typing import cast
 import numpy as np
 
@@ -18,26 +19,16 @@ def get_cost(x1: int, x2: int, y1: int, y2: int, target_x: int, target_y: int) -
 def problem_1() -> None:
     total = 0
     for block in text.split("\n\n"):
-        lines = block.split("\n")
-        x1, y1 = (int(x[2:]) for x in lines[0].split(": ")[1].split(", "))
-        x2, y2 = (int(x[2:]) for x in lines[1].split(": ")[1].split(", "))
-        target_x, target_y = (int(x[2:]) for x in lines[2].split(": ")[1].split(", "))
-
+        x1, y1, x2, y2, target_x, target_y = map(int, re.findall(r"-?\d+", block))
         total += get_cost(x1, x2, y1, y2, target_x, target_y)
-
     print(total)
 
 
 def problem_2() -> None:
     total = 0
     for block in text.split("\n\n"):
-        lines = block.split("\n")
-        x1, y1 = (int(x[2:]) for x in lines[0].split(": ")[1].split(", "))
-        x2, y2 = (int(x[2:]) for x in lines[1].split(": ")[1].split(", "))
-        target_x, target_y = (int(x[2:]) for x in lines[2].split(": ")[1].split(", "))
+        x1, y1, x2, y2, target_x, target_y = map(int, re.findall(r"-?\d+", block))
         target_x += 10000000000000
         target_y += 10000000000000
-
         total += get_cost(x1, x2, y1, y2, target_x, target_y)
-
     print(total)
