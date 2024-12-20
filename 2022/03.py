f1bf65a51c253f4f9bfa4d6232e62db22d17f956
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-20 23:42:46 +0100

    2022 day 3

---
+++ 2022/03.py
@@ -0,0 +1,26 @@
+from itertools import batched
+
+
+with open("./2022/resources/3.txt") as f:
+    lines = f.read().splitlines()
+
+
+def problem_1() -> None:
+    total = 0
+    for line in lines:
+        left, right = set(line[: len(line) // 2]), set(line[len(line) // 2 :])
+        common = (left & right).pop()
+        total += ord(common.lower()) - ord("a") + 1
+        if common.isupper():
+            total += 26
+    print(total)
+
+
+def problem_2() -> None:
+    total = 0
+    for line_1, line_2, line_3 in batched(lines, 3):
+        common = (set(line_1) & set(line_2) & set(line_3)).pop()
+        total += ord(common.lower()) - ord("a") + 1
+        if common.isupper():
+            total += 26
+    print(total)
