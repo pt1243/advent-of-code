4615d419e266e71a25ed54c8b124a1368eea586c
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-20 23:43:04 +0100

    2022 day 5

---
+++ 2022/05.py
@@ -0,0 +1,36 @@
+with open("./2022/resources/5.txt") as f:
+    text = f.read().rstrip()
+
+
+def problem_1() -> None:
+    starting_stacks, moves = text.split("\n\n")
+    lines = starting_stacks.split("\n")[:-1]
+    num_stacks = max(len(line) for line in lines) // 4 + 1
+    stacks: list[list[str]] = [[] for _ in range(num_stacks)]
+    for line in reversed(lines):
+        for i, char in enumerate(line[1::4]):
+            if char != " ":
+                stacks[i].append(char)
+
+    for line in moves.split("\n"):
+        num, source, destination = (int(x) for x in line.split()[1::2])
+        for _ in range(num):
+            stacks[destination - 1].append(stacks[source - 1].pop())
+    print("".join(stack[-1] for stack in stacks))
+
+
+def problem_2() -> None:
+    starting_stacks, moves = text.split("\n\n")
+    lines = starting_stacks.split("\n")[:-1]
+    num_stacks = max(len(line) for line in lines) // 4 + 1
+    stacks: list[list[str]] = [[] for _ in range(num_stacks)]
+    for line in reversed(lines):
+        for i, char in enumerate(line[1::4]):
+            if char != " ":
+                stacks[i].append(char)
+
+    for line in moves.split("\n"):
+        num, source, destination = (int(x) for x in line.split()[1::2])
+        stacks[destination - 1].extend(stacks[source - 1][-num:])
+        stacks[source - 1] = stacks[source - 1][:-num]
+    print("".join(stack[-1] for stack in stacks))
