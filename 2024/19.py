b6c1ea40ac07194563bc5f1f97640648aeaa0e38
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-20 22:02:49 +0100

    2024 day 19

---
+++ 2024/19.py
@@ -0,0 +1,28 @@
+from functools import cache
+
+
+with open("./2024/resources/19.txt") as f:
+    text = f.read().strip()
+
+
+@cache
+def num_ways(target: str, options: tuple[str, ...]) -> int:
+    if not target:
+        return 1
+    total = 0
+    for option in options:
+        if target.startswith(option):
+            total += num_ways(target[len(option) :], options)
+    return total
+
+
+def problem_1() -> None:
+    options_text, targets = text.split("\n\n")
+    options = tuple(options_text.split(", "))
+    print(sum(1 for target in targets.split("\n") if num_ways(target, options)))
+
+
+def problem_2() -> None:
+    options_text, targets = text.split("\n\n")
+    options = tuple(options_text.split(", "))
+    print(sum(num_ways(target, options) for target in targets.split("\n")))
