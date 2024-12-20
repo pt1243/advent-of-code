e0d70b73cbc7f02c445669e1c70a1885035b7835
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-20 23:43:16 +0100

    2022 day 6

---
+++ 2022/06.py
@@ -0,0 +1,19 @@
+from more_itertools import sliding_window
+
+
+with open("./2022/resources/6.txt") as f:
+    text = f.read().strip()
+
+
+def problem_1() -> None:
+    for i, chars in enumerate(sliding_window(text, 4), start=4):
+        if len(set(chars)) == 4:
+            print(i)
+            return
+
+
+def problem_2() -> None:
+    for i, chars in enumerate(sliding_window(text, 14), start=14):
+        if len(set(chars)) == 14:
+            print(i)
+            return
