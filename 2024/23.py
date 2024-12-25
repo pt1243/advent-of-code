49d2c74b782bec8337942f41782b45de250e9e39
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-25 11:46:30 +0100

    use a proper algorithm instead of a greedy one

--- 2024/23.py
+++ 2024/23.py
@@ -28,19 +28,21 @@ def problem_2() -> None:
         connections[left].add(right)
         connections[right].add(left)
 
-    max_length = 0
-    longest_group: set[str] = set()
-    for start in connections:
-        group = {start}
-        while True:
-            for other in connections:
-                if all(other in connections[computer] for computer in group):
-                    group.add(other)
-                    break
-            else:
-                break
-        length = len(group)
-        if length > max_length:
-            longest_group = group
-            max_length = length
-    print(",".join(sorted(longest_group)))
+    largest_clique: set[str] = set()
+    largest_clique_size = 0
+
+    def bron_kerbosch(R: set[str], P: set[str], X: set[str]) -> None:
+        nonlocal largest_clique_size, largest_clique
+        if not P and not X:
+            if len(R) > largest_clique_size:
+                largest_clique_size = len(R)
+                largest_clique = R
+            return
+        u = next(iter(P | X))  # pivot
+        for v in P - connections[u]:
+            bron_kerbosch(R | {v}, P & connections[v], X & connections[v])
+            P -= {v}
+            X |= {v}
+
+    bron_kerbosch(set(), set(connections), set())
+    print(",".join(sorted(largest_clique)))
