89d5f13699620319eee9fc5fd0c81a7434735bb0
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-12-25 10:13:06 +0100

    2024 day 25

---
+++ 2024/25.py
@@ -0,0 +1,17 @@
+from itertools import islice, groupby
+
+
+with open("./2024/resources/25.txt") as f:
+    text = f.read().strip()
+
+
+def problem_1() -> None:
+    keys: list[list[int]] = []
+    locks: list[list[int]] = []
+    for block in text.split("\n\n"):
+        lines = block.split("\n")
+        if all(c == "#" for c in lines[0]):
+            locks.append([len(list(g)) - 1 for col in zip(*lines) for _, g in islice(groupby(col), 1)])
+        else:
+            keys.append([len(list(g)) - 1 for col in zip(*lines) for _, g in islice(groupby(col), 1, 2)])
+    print(sum(all(lock_pin + key_pin <= 5 for lock_pin, key_pin in zip(lock, key)) for lock in locks for key in keys))
