ce388209c0e860485ac92d02656cbc0cc7c02b07
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2025-01-07 14:12:09 +0100

    add helper script to use specialist

---
+++ specialisation.py
@@ -0,0 +1,20 @@
+import argparse
+import specialist
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year")
+    parser.add_argument("day")
+    parser.add_argument("problem")
+    args = parser.parse_args()
+
+    year = args.year
+    day = args.day
+    problem = args.problem
+
+    specialist.main(("--targets", f"{year}/{day.zfill(2)}.py", "main.py", year, day, problem))
+
+
+if __name__ == "__main__":
+    main()
