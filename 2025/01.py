4193cbaac65d9e98aa03b7616999cb197b985f75
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2025-12-02 09:41:29 +1000

    smarter solution for 2025 day 1

--- 2025/01.py
+++ 2025/01.py
@@ -17,13 +17,16 @@ def problem_1() -> None:
 
 def problem_2() -> None:
     current_dial_number = 50
-    on_zero = 0
+    zeros = 0
     for line in lines:
         direction = -1 if line[0] == "L" else 1
         distance = int(line[1:])
-        for _ in range(distance):
-            current_dial_number += direction
-            current_dial_number %= 100
-            if current_dial_number == 0:
-                on_zero += 1
-    print(on_zero)
+        full_turns, rest = divmod(distance, 100)
+        zeros += full_turns
+        turns, new_dial_number = divmod(current_dial_number + rest * direction, 100)
+        if new_dial_number == 0:
+            zeros += 1
+        elif current_dial_number != 0:
+            zeros += abs(turns)
+        current_dial_number = new_dial_number
+    print(zeros)
